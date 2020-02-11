3f8eb732bafaaf5c25368fd2975ecf3d5dd3dba1
Daniel <d.meint@tum.de>
2020-02-11 11:19:12 +0100

    replacement simulation

--- ds2os-topology-replacement/config.py
+++ ds2os-topology-replacement/config.py
@@ -5,6 +5,20 @@ from icarus.util import Tree
 
 import icarus.models as cache
 
+TRACE_PATH    = '/Users/danielmeint/experiments/trace/subTrace2.csv'
+CONTENTS_PATH = '/Users/danielmeint/experiments/trace/contents.txt'
+
+# def read_csv(path):
+#     res = []
+#     with open(path) as file:
+#         reader = csv.DictReader(file)
+#         for row in reader:
+#             res.append(row)
+#     return res
+
+# # needed for MIN policy
+# TRACE = [request['accessedNodeAddress'] for request in read_csv(TRACE_PATH)]
+
 # GENERAL SETTINGS
 
 # Level of logging output
@@ -47,8 +61,8 @@ default['topology']['name'] = 'DS2OS'
 # Set workload
 default['workload'] = {
          'name':            'DS2OS',
-         'reqs_file':       '/Users/danielmeint/experiments/trace/subTrace2.csv',
-         'contents_file':   '/Users/danielmeint/experiments/trace/contents.txt'
+         'reqs_file':       TRACE_PATH,
+         'contents_file':   CONTENTS_PATH
         }
 
 # mindestens 1 objekt pro cache, d.h. 6 objekte; 6/34465 = 0.00017408965
@@ -60,25 +74,31 @@ default['cache_placement']['name'] = 'UNIFORM'
 # Set content placement
 default['content_placement']['name'] = 'DS2OS'
 
-# Set cache replacement policy
-default['cache_policy']['name'] = 'LRU'
+# Set placement strategy
+default['strategy']['name'] = 'LCE'
 
-# caching meta-policies / placement strategies
-STRATEGIES = [
-    'NO_CACHE',        # No caching, shortest-path routing
-    'LCE',             # Leave Copy Everywhere
-    'LCD',
-    'EDGE',
-    'CL4M',            # Betweenness Centrality, Cache Less For More
-    'PROB_CACHE',      # ProbCache
-    'RAND_BERNOULLI',  # Random Bernoulli: cache randomly in caches on path
+# Cache replacement policies
+REPLACEMENT_POLICIES = [
+    # 'MIN',
+    'LRU',
+    'SLRU', # needs at least 2 segments to make sense, i.e. also at least 2 objects in each cache
+    'PERFECT_LFU',
+    'IN_CACHE_LFU',
+    'RAND',
 ]
 
-for strategy in STRATEGIES:
+# problem: we register 'TTL' and pass 'cache' and 'f_time' as arguments, however, the NetworkModel init assumes for every node a cache_size[node] as maxlen parameter, e.g. LruCache(10)
+# we want the call to be ttl_cache(LruCache(10), f_time) but it does ttl_cache(10, LruCache(10), f_time) instead
+# solved: manually changed in network.py
+for policy in REPLACEMENT_POLICIES:
     for network_cache in NETWORK_CACHE:
         experiment = copy.deepcopy(default)
-        experiment['strategy']['name'] = strategy
+        experiment['cache_policy']['name'] = policy
+
         experiment['cache_placement']['network_cache'] = network_cache
-        # experiment['workload']['alpha'] = 1.0
-        experiment['desc'] = f'DS2OS topology, {strategy} placement strategy, LRU replacement, {network_cache} network cache'
+        experiment['desc'] = f'DS2OS topology, LCE placement strategy, {policy} replacement, {network_cache} network cache'
+        # segments must be an integer and 0 < segments <= maxlen
+        # alternatively manually set segments = 1; default is 2
+        if policy == 'SLRU' and network_cache == 0.00018:
+            continue
         EXPERIMENT_QUEUE.append(experiment)
